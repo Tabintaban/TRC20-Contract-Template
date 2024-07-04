1980a4d35685ee8ebc12c6ef11df8500e85b9460
ᠤᠯᠠᠭᠠᠨᠪᠠᠭᠠᠲᠤᠷ <tavintavan@gmail.com>
2024-07-04 16:16:47 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -10,11 +10,17 @@ import "./TRC20Detailed.sol";
  * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `TRC20` functions.
+ *
+ * @title Простой Токен
+ * @dev - Очень простой пример токена TRC20, где все токены предварительно назначаются создателю.
+ * Обратите внимание, что позже они могут распространять эти токены по своему усмотрению, используя "передачу" и другие функции
+ * `TRC20`.
  */
-contract Token is TRC20, TRC20Detailed {
 
+contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
+     * @dev Конструктор, который предоставляет msg.sender все существующие токены.
      */
     constructor () public TRC20Detailed("YourTokenName", "YTN", 18) {
         _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
